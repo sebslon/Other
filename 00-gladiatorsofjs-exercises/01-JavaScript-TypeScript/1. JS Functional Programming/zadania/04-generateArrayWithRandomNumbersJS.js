d0ffcf401865406c4a644ec9c95e59f297103bf0
Sebastian <sebastian.sloniec@gmail.com>
2021-04-24 14:19:34 +0200

    validation

--- 00-gladiatorsofjs-exercises/01-JavaScript-TypeScript/1. JS Functional Programming/zadania/04-generateArrayWithRandomNumbersJS.js
+++ 00-gladiatorsofjs-exercises/01-JavaScript-TypeScript/1. JS Functional Programming/zadania/04-generateArrayWithRandomNumbersJS.js
@@ -1,10 +1,10 @@
 const { randomNumberInRange } = require("./helpers");
 
-function generateArrayWithRandomNumbers(
-  howManyNumbers = 10,
-  min = 1,
-  max = 10
-) {
+function generateArrayWithRandomNumbers(howManyNumbers = 10 , min = 1, max = 10) {
+  if(!Number.isInteger(howManyNumbers) || howManyNumbers < 0) throw new Error('howManyNumbers argument is invalid.');
+  if(!Number.isFinite(min * max) || min < 0 || max < 0) throw new Error('Range numbers are invalid.');
+  if(min > max) throw new Error('Maximum number in range should be higher or equal to minimum.')
+
   const arrayWithRandomNumbers = [];
 
   for (let i = 0; i < howManyNumbers; i++) {
@@ -14,12 +14,9 @@ function generateArrayWithRandomNumbers(
   return arrayWithRandomNumbers;
 }
 
-function generateArrayOfArrays(
-  howManyArrays = 10,
-  howManyNumbers = 10,
-  min = 1,
-  max = 10
-) {
+function generateArrayOfArrays(howManyArrays = 10, howManyNumbers = 10, min = 1, max = 10) {
+  if(!Number.isInteger(howManyArrays) || howManyArrays < 0) throw new Error('howManyArrays argument is invalid.');
+
   const result = [];
 
   for (let i = 0; i < howManyArrays; i++) {
