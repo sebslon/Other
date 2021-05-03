c92201a60698000ab773a2c3933800a0da0d815b
Sebastian <sebastian.sloniec@gmail.com>
2021-05-03 17:30:29 +0200

    Validate cartItem

--- 00-gladiatorsofjs-exercises/01-JavaScript-TypeScript/2. JS Object-oriented Programming/solutions/solutions-JS/Cart/CartItemJS.js
+++ 00-gladiatorsofjs-exercises/01-JavaScript-TypeScript/2. JS Object-oriented Programming/solutions/solutions-JS/Cart/CartItemJS.js
@@ -1,32 +1,48 @@
 const { v4: uuid } = require("uuid");
+const Validator = require("../utils/Validator");
 
 class CartItem {
-  constructor(name, category, price, discount = "0%") {
+  constructor(name, category, price, discount = 0) {
     this._id = uuid();
-    this._name = name;
-    this._category = category;
-    this._price = price;
-    this._discount = discount;
+
+    Validator.check("Name", name).isNotEmpty().isString().min(5);
+    Validator.check("Category", category).isNotEmpty().isString();
+    Validator.check("Price", price).isNumber().min(0);
+    Validator.check("Discount", discount).isNumber().between(0, 1);
+
+    this.name = name;
+    this.category = category;
+    this.price = price;
+    this.discount = discount;
+    this.quantity = 0;
   }
 
-  set setPrice(price) {
-    return (this._price = price);
-  }
+  change(property, newValue) {
+    switch (property) {
+      case "name":
+        Validator.check("Name", newValue).isNotEmpty().isString().min(5);
+        return (this.name = newValue);
+      case "category":
+        Validator.check("Category", newValue).isNotEmpty().isString();
+        return (this.category = newValue);
+      case "price":
+        Validator.check("Price", newValue).isNumber().min(0);
+        return (this.price = newValue);
+      case "discount":
+        Validator.check("Discount", newValue).isNumber().between(0, 1);
+        return (this.discount = newValue);
+      case "quantity":
+        Validator.check("Quantity", newValue).isNumber().min(0);
+        return (this.quantity = newValue);
 
-  set setDiscount(discount) {
-    return (this._discount = discount);
-  }
-
-  set changeName(name) {
-    this._name = name;
-  }
-
-  set newCategory(category) {
-    this._category = category;
+      default:
+        throw new Error(property + " property doesn't exist on CartItem");
+    }
   }
 }
 
 module.exports = CartItem;
+
 // Ma miec: Nazwę, Kategorię, Cenę, Rabat % na przedmiot, uuid
 // Ma umożliwiać:
 // - modyfikować cenę przedmiotu
--- 00-gladiatorsofjs-exercises/01-JavaScript-TypeScript/2. JS Object-oriented Programming/solutions/solutions-JS/utils/Validator.js
+++ 00-gladiatorsofjs-exercises/01-JavaScript-TypeScript/2. JS Object-oriented Programming/solutions/solutions-JS/utils/Validator.js
@@ -9,24 +9,77 @@ class Validator {
   }
 
   static isNotEmpty() {
-    if(this.value.length === 0) {
-      throw new Error(this.key + ' should not be empty.')
+    if (this.value.length === 0) {
+      throw new Error(this.key + " should not be empty.");
     }
     return this;
   }
-  
+
   static isString() {
-    if(!(typeof this.value === 'string')) {
-      throw new Error(this.key + ' should be a string.')
-    };
+    if (typeof this.value !== "string") {
+      throw new Error(this.key + " should be a string.");
+    }
+    return this;
+  }
+
+  static isArray() {
+    if (!Array.isArray(this.value)) {
+      throw new Error(this.key + " is not an array.");
+    }
     return this;
   }
 
   static isNumber() {
-    if(!(typeof this.value === 'number')) {
-      throw new Error(this.key + ' should be a number.')
-    };
+    if (!(typeof this.value === "number")) {
+      throw new Error(this.key + " should be a number.");
+    }
     return this;
+  }
+
+  static min(minValue) {
+    if (typeof minValue !== "number") {
+      throw new Error("Minimum value should be a number");
+    }
+
+    if (typeof this.value === "string" || Array.isArray(this.value)) {
+      this.value = this.value.length;
+      this.key = this.key + " length";
+    }
+
+    if (this.isNumber(this.value) && this.value >= minValue) {
+      return this;
+    }
+
+    throw new Error(`${this.key} should be greater than ${minValue}`);
+  }
+
+  static max(maxValue) {
+    if (typeof maxValue !== "number") {
+      throw new Error("Maximum value should be a number");
+    }
+
+    if (typeof this.value === "string" || Array.isArray(this.value)) {
+      this.value = this.value.length;
+      this.key = this.key + " length";
+    }
+
+    if (this.isNumber(this.value) && this.value <= maxValue) {
+      return this;
+    }
+
+    throw new Error(`${this.key} should be lower than ${maxValue}`);
+  }
+
+  static between(min, max) {
+    if (typeof this.value === "string" || Array.isArray(this.value)) {
+      this.value = this.value.length;
+      this.key = this.key + " length";
+    }
+
+    if (this.isNumber(this.value) && this.value <= max && this.value >= min) {
+      return this;
+    }
+    throw new Error(`${this.key} should be between ${min} and ${max}`);
   }
 }
 
