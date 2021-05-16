73340ec498bb93a2a79f21b3f393c9c37a2a6722
Sebastian <sebastian.sloniec@gmail.com>
2021-05-16 20:53:37 +0200

    Refactor pagination fn - extract validation logic, add types

--- 00-gladiatorsofjs-exercises/01-JavaScript-TypeScript/1. JS Functional Programming/solutions/08-pagination.ts
+++ 00-gladiatorsofjs-exercises/01-JavaScript-TypeScript/1. JS Functional Programming/solutions/08-pagination.ts
@@ -1,28 +1,44 @@
-const paginateArray = <T>(dataEntries: T[], settings = { actualPageIndex: 0, entriesOnPage: 5 }) => {
-  const inputIsNotAnArray = !Array.isArray(dataEntries);
+type Settings = { actualPageIndex: number; entriesOnPage: number };
 
-  if (inputIsNotAnArray) {
-    throw new TypeError("Given input is not an array.");
-  }
+const defaultSettings: Settings = {
+  actualPageIndex: 0,
+  entriesOnPage: 5,
+};
 
-  const {actualPageIndex, entriesOnPage} = settings;
-
-  if (actualPageIndex < 0 || !Number.isInteger(actualPageIndex)) {
-    throw new Error('actualPageIndex is invalid number.');
-  }
+export const paginateArray = <T>(
+  dataEntries: T[],
+  settings: Settings = defaultSettings
+): T[] => {
+  validateInput(dataEntries, settings);
   
-  if (entriesOnPage < 1 || !Number.isInteger(entriesOnPage)) {
-    throw new Error('entriesOnPage is invalid number.');
-  }
+  const { actualPageIndex, entriesOnPage } = settings;
 
   const firstElement = actualPageIndex * entriesOnPage;
   const lastElement = firstElement + entriesOnPage;
 
   const entriesOnSelectedPage = dataEntries.slice(firstElement, lastElement);
 
-  return entriesOnSelectedPage
+  return entriesOnSelectedPage;
 };
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-const result = paginateArray(arr, { actualPageIndex: 1, entriesOnPage: 3 });
-console.log(result)
+const validateInput = <T>(dataEntries: T[], settings: Settings) => {
+  const inputIsNotAnArray = !Array.isArray(dataEntries);
+
+  const { actualPageIndex, entriesOnPage } = settings;
+
+  if (inputIsNotAnArray) {
+    throw new TypeError("Given input is not an array.");
+  }
+
+  if (actualPageIndex < 0 || !Number.isInteger(actualPageIndex)) {
+    throw new Error("actualPageIndex is invalid number.");
+  }
+
+  if (entriesOnPage < 1 || !Number.isInteger(entriesOnPage)) {
+    throw new Error("entriesOnPage is invalid number.");
+  }
+};
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// const result = paginateArray(arr);
+// console.log(result);
