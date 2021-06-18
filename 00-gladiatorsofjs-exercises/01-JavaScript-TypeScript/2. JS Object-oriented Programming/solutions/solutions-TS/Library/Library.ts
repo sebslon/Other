3df6167cd94dbdccf5770ee8c94745eca7e03b5b
Sebastian <sebastian.sloniec@gmail.com>
2021-06-18 20:42:58 +0200

    OOP: Library - fix

--- 00-gladiatorsofjs-exercises/01-JavaScript-TypeScript/2. JS Object-oriented Programming/solutions/solutions-TS/Library/Library.ts
+++ 00-gladiatorsofjs-exercises/01-JavaScript-TypeScript/2. JS Object-oriented Programming/solutions/solutions-TS/Library/Library.ts
@@ -22,17 +22,10 @@ class Library implements ILibrary {
     this.borrowedBooks = [];
   }
 
-  findBook(book: Book) {
-    const index = this.books.indexOf(book);
-    if(index === -1) {
-      throw new Error('Book not found');
-    }
-    return this.books[index];
-  }
-
   addBook(book: Book) {
-    if(this.books.includes(book)) {
-      throw new Error('Book is already in the library.');
+    const bookInLibrary = this.books.indexOf(book) !== -1;
+    if(bookInLibrary) {
+      throw new Error("Book is already in the library.")
     }
 
     this.books.push(book);
@@ -40,9 +33,9 @@ class Library implements ILibrary {
 
   removeBook(book: Book) {
     const bookIndex = this.books.indexOf(book);
-    
+
     if (bookIndex === -1) {
-      throw new Error('This book is not in the library.')
+      throw new Error("This book is not in the library.");
     }
 
     return this.books.splice(bookIndex, 1);
@@ -51,8 +44,8 @@ class Library implements ILibrary {
   createBooking(book: Book, user: LibraryUser) {
     const bookInLibrary = this.findBook(book);
 
-    if(!bookInLibrary) {
-      throw new Error("Sorry, we don't have this book.")
+    if (!bookInLibrary) {
+      throw new Error("Sorry, we don't have this book.");
     }
 
     const booking = new Booking(user, book);
@@ -69,14 +62,47 @@ class Library implements ILibrary {
     const penalty = this.bookings[bookingIndex].calculatePenalty(date);
 
     if (penalty !== 0) {
-      throw new Error('You have to pay penalty: ' + penalty);
+      throw new Error("You have to pay penalty: " + penalty);
     }
 
-    const [ returnedBook ] = this.borrowedBooks.filter(book => book._id === booking.bookId);
+    const [returnedBook] = this.borrowedBooks.filter(
+      (book) => book._id === booking.bookId
+    );
     this.books.push(returnedBook);
-    this.bookings.splice(bookingIndex, 1)
+    this.bookings.splice(bookingIndex, 1);
 
     return "Successfully returned";
   }
+
+  findBook(book: Book) {
+    const index = this.books.indexOf(book);
+    if (index === -1) {
+      throw new Error("Book not found");
+    }
+    return this.books[index];
+  }
 }
 
+// Ma miec: listę książek, listę wypożyczeń oraz listę wypożyczonych książek -- DONE
+// Ma umożliwiać:
+// - dodawanie książek do listy
+// - usuwanie książek z listy
+// - wypożyczanie książki dla usera X
+// - oddanie wypożyczania książki
+
+const library = new Library("Krakowska");
+const book = new Book("Książka", "autor");
+const book1 = new Book("Książkax", "autor");
+const book2 = new Book("Książkaxy", "autor");
+
+const seba = new LibraryUser("seba", "sloniec");
+
+library.addBook(book);
+library.addBook(book1);
+library.addBook(book2);
+
+const booking = library.createBooking(book, seba);
+library.finishBooking(booking, new Date(2020, 14, 14));
+
+console.log(library.bookings);
+console.log(library.books);
