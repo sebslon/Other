1c2a90f4800f92cae4b41021e2d9d61d09667d16
Sebastian <sebastian.sloniec@gmail.com>
2021-09-23 10:53:23 +0200

    DPatterns: Strategy Pattern concept impl

---
+++ 02-design-patterns-JS/Behavioral/01-Strategy/01-strategy-concept.ts
@@ -0,0 +1,53 @@
+// Lets you define a family of different algorithms, each in separate class - which you can use interchangeable (in context) to solve a problem in a different ways.
+
+interface Strategy {
+  doAlgorithm(data: string[]): string[];
+}
+
+// Context has reference to one of Strategies, it should work with all strategies by Strategy interface
+class Context {
+  private strategy: Strategy;
+
+  constructor(strategy: Strategy) {
+    this.strategy = strategy;
+  }
+
+  //Allow context to change it's strategy at runtime.
+  setStrategy(strategy: Strategy) {
+    this.strategy = strategy;
+  }
+
+  // sorting a string array - for example
+  doBusinessLogic(): string[] {
+    const result = this.strategy.doAlgorithm(["a", "e", "d", "c", "b", "f"]);
+    
+    return result;
+  }
+}
+
+// Concrete Strategies
+class NormalSortStrategy implements Strategy {
+  doAlgorithm(data: string[]): string[] {
+    console.log("Normal sorting");
+
+    return data.sort();
+  }
+}
+
+class ReverseSortStrategy implements Strategy {
+  doAlgorithm(data: string[]): string[] {
+    console.log("Reverse sorting");
+    
+    return data.reverse();
+  }
+}
+
+/*
+Creating a context with default strategy, then changing strategy if needed.
+*/
+
+const context = new Context(new NormalSortStrategy());
+console.log(context.doBusinessLogic());
+
+context.setStrategy(new ReverseSortStrategy());
+console.log(context.doBusinessLogic());
