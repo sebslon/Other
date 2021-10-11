6ef22d945cd970e3f02369dde1aeb5bcb4821573
Sebastian <sebastian.sloniec@gmail.com>
2021-10-11 20:40:03 +0200

    MasonryGrid: Create Masonry Grid

--- 00-gladiatorsofjs-exercises/02-React-TS/solutions/src/pages/1-ReactTS_Fundamentals/9-MasonryGrid/components/MasonryGridCell/index.tsx
+++ 00-gladiatorsofjs-exercises/02-React-TS/solutions/src/pages/1-ReactTS_Fundamentals/9-MasonryGrid/components/MasonryGridCell/index.tsx
@@ -1,7 +1,7 @@
-export const MasonryGridCell = ({ key, src }: any) => {
-  return (
-    <div>
-      
-    </div>
-  )
+interface MasonryGridCellProps {
+  photoUrl: string;
 }
+
+export const MasonryGridCell = ({ photoUrl }: MasonryGridCellProps) => (
+  <img src={photoUrl} width="100%" height="auto" alt="masonry item" />
+);
