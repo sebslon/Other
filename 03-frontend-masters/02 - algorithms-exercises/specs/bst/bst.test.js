81575abd39f4acd6d6c3a78d94f52b4f57b8e1fe
Sebastian <sebastian.sloniec@gmail.com>
2021-10-18 16:11:16 +0200

    CS: BST and AVL self balancing tree

--- 03-frontend-masters/02 - algorithms-exercises/specs/bst/bst.test.js
+++ 03-frontend-masters/02 - algorithms-exercises/specs/bst/bst.test.js
@@ -17,17 +17,59 @@ right - Node/object - the right node which itself may be another tree
 */
 
 class Tree {
-  // code goes here
+  constructor() {
+    this.root = null;
+  }
+
+  add(value) {
+    if (!this.root) {
+      this.root = new Node(value);
+    } else {
+      let current = this.root;
+
+      while (true) {
+        if (current.value > value) {
+          // GO LEFT
+          if (current.left) {
+            current = current.left;
+          } else {
+            current.left = new Node(value);
+            return;
+          }
+          //
+        } else {
+          // GO RIGHT
+          if (current.right) {
+            current = current.right;
+          } else {
+            current.right = new Node(value);
+            return;
+          }
+          ///
+        }
+      }
+    }
+
+    return this;
+  }
+
+  // For visualization purposes
+  toObject() {
+    return this.root;
+  }
 }
 
-// you might consider using a Node class too
-// class Node {
-//   // code maybe goes here
-// }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
 // unit tests
 // do not modify the below code
-describe.skip("Binary Search Tree", function () {
+describe("Binary Search Tree", function () {
   it("creates a correct tree", () => {
     const nums = [3, 7, 4, 6, 5, 1, 10, 2, 9, 8];
     const tree = new Tree();
