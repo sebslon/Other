c66194fcefb196553f73eaccf816154552b13b1a
Sebastian <sebastian.sloniec@gmail.com>
2021-10-22 20:27:44 +0200

    FUNDAMENTALS: DynamicSwitch validating array of conditions/callbacks class

---
+++ 00-gladiatorsofjs-exercises/01-JavaScript-TypeScript/2. JS Object-oriented Programming/solutions/solutions-TS/DynamicSwitch/DynamicSwitch.ts
@@ -0,0 +1,33 @@
+interface Case {
+  condition: boolean;
+  callback: () => void;
+}
+
+class Switch {
+  cases: Case[] = [];
+
+  add(condition: boolean, callback: () => void) {
+    this.cases.push({condition, callback});
+  }
+
+  isValid(): boolean {
+    let isValid = true;
+
+    for (let i = 0; i < this.cases.length; i++) {
+      const isConditionFilled = this.cases[i].condition;
+
+      if(!isConditionFilled) {
+        this.cases[i].callback();
+      };
+
+      isValid = false;
+    }
+    
+    this.cases = [];
+    return isValid;
+  }
+
+  isEmpty() {
+    return this.cases.length === 0;
+  }
+}
