72dec0f749b3f232c2db8ff1df37b48ffc91ebdb
Sebastian <sebastian.sloniec@gmail.com>
2022-01-04 22:06:47 +0100

    FEM-xstate: create drag and drop with actions

--- 03-frontend-masters/07 - xstate-workshop/src/04/index.js
+++ 03-frontend-masters/07 - xstate-workshop/src/04/index.js
@@ -1,48 +1,52 @@
-import { createMachine, interpret } from 'xstate';
+import { createMachine, interpret } from "xstate";
 
-const elBox = document.querySelector('#box');
+const elBox = document.querySelector("#box");
 
 const setPoint = (context, event) => {
-  // Set the data-point attribute of `elBox`
-  // ...
+  elBox.dataset.point = `(${event.clientX}, ${event.clientY})`;
 };
 
-const machine = createMachine({
-  initial: 'idle',
-  states: {
-    idle: {
-      on: {
-        mousedown: {
-          // Add your action here
-          // ...
-          target: 'dragging',
+const machine = createMachine(
+  {
+    initial: "idle",
+    states: {
+      idle: {
+        on: {
+          mousedown: {
+            // actions: "dragging",
+            actions: [setPoint],
+            target: "dragging",
+          },
         },
       },
-    },
-    dragging: {
-      on: {
-        mouseup: {
-          target: 'idle',
+      dragging: {
+        on: {
+          mouseup: {
+            target: "idle",
+          },
         },
       },
     },
   },
-});
+  {
+    actions: {
+      dragging: (context, event) => setPoint(context, event),
+      // setPoint: (context, event) =>
+      //   console.log("overrided setPoint for testing for example"),
+    },
+  }
+);
 
 const service = interpret(machine);
 
 service.onTransition((state) => {
-  console.log(state);
-
   elBox.dataset.state = state.value;
 });
 
 service.start();
 
-elBox.addEventListener('mousedown', (event) => {
+elBox.addEventListener("mousedown", (event) => {
   service.send(event);
 });
 
-elBox.addEventListener('mouseup', (event) => {
-  service.send(event);
-});
+elBox.addEventListener("mouseup", service.send); // point-free (no args) style
