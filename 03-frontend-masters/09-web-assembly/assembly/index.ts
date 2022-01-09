82bd7dde886881d69c90dace72b9ed5e80042f9c
Sebastian <sebastian.sloniec@gmail.com>
2022-01-09 18:26:59 +0100

    FEM-WASM: add wasm loader for utility functions

--- 03-frontend-masters/09-web-assembly/assembly/index.ts
+++ 03-frontend-masters/09-web-assembly/assembly/index.ts
@@ -1,3 +1,25 @@
+declare function log(n: i32): void;
+
 export function minusOne(n: i32): i32 {
+  log(n);
+  if (n === 44) {
+    abort(); //example of imported functions
+  }
   return n - 1;
+}
+
+export function fizzbuzz(n: i32): String | null {
+  if (n % 15 === 0) {
+    return "FizzBuzz";
+  }
+
+  if (n % 3 === 0) {
+    return "Fizz";
+  }
+
+  if (n % 5 === 0) {
+    return "Buzz";
+  }
+
+  return null;
 }
--- 03-frontend-masters/09-web-assembly/js/loader.js
+++ 03-frontend-masters/09-web-assembly/js/loader.js
@@ -1,24 +1,41 @@
 class WasmLoader {
-  constructor() {}
+  constructor() {
+    this._imports = {
+      env: {
+        abort() {
+          throw new Error("Abort called from wasm file");
+        },
+      },
+      index: {
+        log(n) {
+          console.log(n);
+        },
+      },
+    };
+  }
 
-  async wasm(path) {
+  async wasm(path, imports = this._imports) {
     console.log(`Fetching ${path}`);
 
-    if (!WebAssembly.instantiateStreaming) {
-      return this.wasmFallback(path);
+    // WebAssembly - loader.
+    if (!loader.instantiateStreaming) {
+      return this.wasmFallback(path, imports);
     }
 
-    const { instance } = await WebAssembly.instantiateStreaming(fetch(path));
+    const { instance } = await loader.instantiateStreaming(
+      fetch(path),
+      imports
+    );
 
     return instance?.exports;
   }
 
-  async wasmFallback(path) {
+  async wasmFallback(path, imports) {
     console.log(`Fallback fetching ${path}`);
 
     const response = await fetch(path);
     const bytes = await response?.arrayBuffer();
-    const { instance } = WebAssembly.instantiateStreaming(bytes);
+    const { instance } = loader.instantiateStreaming(bytes, imports);
 
     return instance?.exports;
   }
