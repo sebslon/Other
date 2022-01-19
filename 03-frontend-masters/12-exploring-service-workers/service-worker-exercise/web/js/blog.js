1357b527796613af92cb91a95b2fa2180ffa7b8f
Sebastian <sebastian.sloniec@gmail.com>
2022-01-19 20:55:33 +0100

    FEM-workers: basic configuration connection with sw

--- 03-frontend-masters/12-exploring-service-workers/service-worker-exercise/web/js/blog.js
+++ 03-frontend-masters/12-exploring-service-workers/service-worker-exercise/web/js/blog.js
@@ -1,14 +1,52 @@
 (function Blog() {
   "use strict";
 
-  var offlineIcon;
-  var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "");
+  let offlineIcon;
+  let isOnline = "onLine" in navigator ? navigator.onLine : true;
+  let usingSW = "serviceWorker" in navigator;
+  let swRegistration;
+  let svcworker;
+
+  const isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "");
 
   document.addEventListener("DOMContentLoaded", ready, false);
 
+  initServiceWorker().catch(console.error);
   // **********************************
 
   function ready() {
     offlineIcon = document.getElementById("connectivity-status");
+
+    if (!isOnline) {
+      offlineIcon.classList.remove("hidden");
+    }
+
+    window.addEventListener("online", function online() {
+      offlineIcon.classList.add("hidden");
+      isOnline = true;
+    });
+
+    window.addEventListener("offline", function offline() {
+      offlineIcon.classList.remove("hidden");
+      isOnline = false;
+    });
+  }
+
+  async function initServiceWorker() {
+    swRegistration = await navigator.serviceWorker.register("/js/sw.js", {
+      updateViaCache: "none",
+    }); // start up and access service worker
+
+    svcworker =
+      swRegistration.installing ||
+      swRegistration.waiting ||
+      swRegistration.active;
+
+    navigator.serviceWorker.addEventListener(
+      "controllerchange",
+      function onController() {
+        svcworker = navigator.serviceWorker.controller;
+      }
+    );
   }
 })();
