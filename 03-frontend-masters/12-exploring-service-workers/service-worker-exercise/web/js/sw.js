f8bc442a5ebe5220177af9bad31652cb46509136
Sebastian <sebastian.sloniec@gmail.com>
2022-01-19 21:36:47 +0100

    FEM-workers: setup communication

--- 03-frontend-masters/12-exploring-service-workers/service-worker-exercise/web/js/sw.js
+++ 03-frontend-masters/12-exploring-service-workers/service-worker-exercise/web/js/sw.js
@@ -3,10 +3,13 @@
 // TODO
 // Be aware of global scope life time in service workers
 
-const version = 1;
+const version = 2;
+let isOnline = true;
+let isLoggedIn = false;
 
 self.addEventListener("install", onInstall);
 self.addEventListener("activate", onActivated);
+self.addEventListener("message", onMessage);
 
 main().catch(console.error);
 
@@ -14,11 +17,33 @@ main().catch(console.error);
 
 async function main() {
   console.log(`Service worker (${version}) is started.`);
+
+  await sendMessage({ requestStatusUpdate: true });
 }
 
 async function onInstall(evt) {
   console.log(`Service worker (${version}) is installed.`);
   self.skipWaiting();
+}
+
+async function sendMessage(msg) {
+  const allClients = await clients.matchAll({ includeUncontrolled: true }); // get all clients
+  return Promise.all(
+    allClients.map(function clientMsg(client) {
+      const chan = new MessageChannel();
+      chan.port1.onmessage = onMessage; // send message port1
+      return client.postMessage(msg, [chan.port2]); // receive message port2
+    })
+  );
+}
+
+function onMessage({ data }) {
+  if (data.statusUpdate) {
+    ({ isOnline, isLoggedIn } = data.statusUpdate);
+    console.log(
+      `Service worker (${version}) status update, isOnline: ${isOnline}, isLoggedIn: ${isLoggedIn}`
+    );
+  }
 }
 
 function onActivated(evt) {
