0383b6a3301fc18ac64abc93a6128fe665545076
Sebastian <sebastian.sloniec@gmail.com>
2022-01-19 19:16:24 +0100

    format

--- 03-frontend-masters/12-exploring-service-workers/web-worker-exercise/web/js/home.js
+++ 03-frontend-masters/12-exploring-service-workers/web-worker-exercise/web/js/home.js
@@ -1,49 +1,55 @@
-(function Home(){
-	"use strict";
+(function Home() {
+  "use strict";
 
-	var startStopBtn;
-	var fibsList;
+  var startStopBtn;
+  var fibsList;
+  var worker;
 
-	document.addEventListener("DOMContentLoaded",ready,false);
+  document.addEventListener("DOMContentLoaded", ready, false);
 
+  // **********************************
 
-	// **********************************
+  function ready() {
+    startStopBtn = document.getElementById("start-stop-btn");
+    fibsList = document.getElementById("fibs");
 
-	function ready() {
-		startStopBtn = document.getElementById("start-stop-btn");
-		fibsList = document.getElementById("fibs");
+    startStopBtn.addEventListener("click", startFibs, false);
+  }
 
-		startStopBtn.addEventListener("click",startFibs,false);
-	}
+  function renderFib(num, fib) {
+    var p = document.createElement("div");
+    p.innerText = `Fib(${num}): ${fib}`;
+    if (fibsList.childNodes.length > 0) {
+      fibsList.insertBefore(p, fibsList.childNodes[0]);
+    } else {
+      fibsList.appendChild(p);
+    }
+  }
 
-	function renderFib(num,fib) {
-		var p = document.createElement("div");
-		p.innerText = `Fib(${num}): ${fib}`;
-		if (fibsList.childNodes.length > 0) {
-			fibsList.insertBefore(p,fibsList.childNodes[0]);
-		}
-		else {
-			fibsList.appendChild(p);
-		}
-	}
+  function startFibs() {
+    startStopBtn.removeEventListener("click", startFibs, false);
+    startStopBtn.addEventListener("click", stopFibs, false);
 
-	function startFibs() {
-		startStopBtn.removeEventListener("click",startFibs,false);
-		startStopBtn.addEventListener("click",stopFibs,false);
+    startStopBtn.innerText = "Stop";
+    fibsList.innerHTML = "";
 
-		startStopBtn.innerText = "Stop";
-		fibsList.innerHTML = "";
+    // TODO - Create Worker
 
-		// TODO
-	}
+    worker = new Worker("/js/worker.js");
 
-	function stopFibs() {
-		startStopBtn.removeEventListener("click",stopFibs,false);
-		startStopBtn.addEventListener("click",startFibs,false);
+    worker.addEventListener("message", onMessage);
+  }
 
-		startStopBtn.innerText = "Start";
+  function stopFibs() {
+    startStopBtn.removeEventListener("click", stopFibs, false);
+    startStopBtn.addEventListener("click", startFibs, false);
 
-		// TODO
-	}
+    startStopBtn.innerText = "Start";
 
+    // TODO
+  }
+
+  function onMessage(evt) {
+    console.log(evt.data);
+  }
 })();
--- 03-frontend-masters/12-exploring-service-workers/web-worker-exercise/web/js/worker.js
+++ 03-frontend-masters/12-exploring-service-workers/web-worker-exercise/web/js/worker.js
@@ -4,11 +4,13 @@ var curFib = 0;
 
 // TODO
 
+self.postMessage("Hello from the web worker");
+
 // **********************************
 
 function fib(n) {
-	if (n < 2) {
-		return n;
-	}
-	return fib(n-1) + fib(n-2);
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
 }
