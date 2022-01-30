c2036f4bc2fb81566d07fe711078c2a45e871440
Sebastian <sebastian.sloniec@gmail.com>
2022-01-30 13:04:37 +0100

    NFT: Add an image to NFT with token metadata

--- 05-nft-ticketing-system/packages/hardhat/contracts/NFTixBooth.sol
+++ 05-nft-ticketing-system/packages/hardhat/contracts/NFTixBooth.sol
@@ -7,6 +7,8 @@ pragma solidity >=0.8.0 <0.9.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "./Base64.sol";
 
 contract NFTixBooth is ERC721URIStorage {
     using Counters for Counters.Counter;
@@ -24,7 +26,40 @@ contract NFTixBooth is ERC721URIStorage {
 
     function mint() public {
         require(availableTickets > 0, "Not enough tickets.");
+
+        // --- embedding an image with token metadata [START]
+        string[3] memory svg;
+        svg[
+            0
+        ] = '<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><text y="50">';
+        svg[1] = Strings.toString(currentId.current());
+        svg[2] = "</text></svg>";
+
+        string memory image = string(abi.encodePacked(svg[0], svg[1], svg[2]));
+        string memory encodedImage = Base64.encode(bytes(image));
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{ "name": "NFTix #',
+                        Strings.toString(currentId.current()),
+                        '", "description": "A NFT-powered ticketing system", ',
+                        '"traits": [{ "trait_type": "Checked In", "value": "false" }, { "trait_type": "Purchased", "value": "true" }], ',
+                        '"image": "data:image/svg+xml;base64,',
+                        encodedImage,
+                        '" }'
+                    )
+                )
+            )
+        );
+
+        string memory tokenURI = string(
+            abi.encodePacked("data:application.json; base64,", json)
+        );
+        // --- embedding an image with token metadata [END]
+
         _safeMint(msg.sender, currentId.current());
+        _setTokenURI(currentId.current(), tokenURI); // Apply image to the token
 
         currentId.increment();
         availableTickets = availableTickets - 1;
