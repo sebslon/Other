60ce85317447dc92ea9cc25c22214acc048519e4
Sebastian <sebastian.sloniec@gmail.com>
2022-02-02 19:48:22 +0100

    EAP: refactor home compoonent - SRP

--- 03-frontend-masters/17-enterprise-architecture-patterns/apps/micro/src/app/home/home.component.ts
+++ 03-frontend-masters/17-enterprise-architecture-patterns/apps/micro/src/app/home/home.component.ts
@@ -1,34 +1,51 @@
-import { Component } from '@angular/core';
-import { Widget } from '@fem/api-interfaces';
-import { v4 as uuidv4 } from 'uuid';
+import { Component } from "@angular/core";
+import { Widget } from "@fem/api-interfaces";
+import { v4 as uuidv4 } from "uuid";
 
 @Component({
-  selector: 'fem-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  selector: "fem-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent {
   price;
   mode;
   widgets: Widget[];
 
-  updateWidgetsAndRecalculateCost(widget: Widget) {
-    switch (this.mode) {
-      case 'create':
-        const newWidget = Object.assign({}, widget, { id: uuidv4() });
-        this.widgets = [...this.widgets, newWidget];
-        break;
-      case 'update':
-        this.widgets = this.widgets.map(_widget => widget.id === _widget.id
-          ? Object.assign({}, widget) : _widget);
-        break;
-      case 'delete':
-        this.widgets = this.widgets.filter(_widget => widget.id !== _widget.id);
-        break;
+  reCalculateTotal(mode: string, widgets: Widget[], widget: Widget) {
+    this.widgets = this.updateWidgets(mode, widgets, widget);
+    this.price = this.getTotalPrice(widgets);
+  }
+
+  getTotalPrice(widgets) {
+    return widgets.reduce((acc, curr) => acc + curr.price, 0);
+  }
+
+  addWidget(widgets, widget: Widget) {
+    const newWidget = Object.assign({}, widget, { id: uuidv4() });
+    return [...widgets, newWidget];
+  }
+
+  updateWidget(widgets: Widget[], widget: Widget) {
+    return (widgets = widgets.map((_widget) =>
+      widget.id === _widget.id ? Object.assign({}, widget) : _widget
+    ));
+  }
+
+  updateWidgets(mode: string, widgets, widget: Widget) {
+    switch (mode) {
+      case "create":
+        return this.addWidget(widgets, widget);
+      case "update":
+        return this.updateWidget(widgets, widget);
+      case "delete":
+        return this.deleteWidget(widgets, widget);
       default:
         break;
     }
-    return this.widgets.reduce((acc, curr) => acc + curr.price, 0);
   }
 
+  deleteWidget(widgets, widget: Widget) {
+    return (widgets = widgets.filter((_widget) => widget.id !== _widget.id));
+  }
 }
