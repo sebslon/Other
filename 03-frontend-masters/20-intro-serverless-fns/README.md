0f19b41db5c09a645c888576554ecdb001102efa
Sebastian <sebastian.sloniec@gmail.com>
2022-02-16 19:49:04 +0100

    SERVERLESS: Use hasura to fetch and save movies data

--- 03-frontend-masters/20-intro-serverless-fns/README.md
+++ 03-frontend-masters/20-intro-serverless-fns/README.md
@@ -14,3 +14,9 @@ This is the source code for a Frontend Masters course with Jason Lengstorf about
 ## License
 
 <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
+
+### Setup
+
+OMDB_API_KEY
+HASURA_API_URL
+HASURA_ADMIN_SECRET
--- 03-frontend-masters/20-intro-serverless-fns/functions/movies.js
+++ 03-frontend-masters/20-intro-serverless-fns/functions/movies.js
@@ -2,33 +2,47 @@ require("dotenv").config();
 
 const { URL } = require("url");
 const fetch = require("node-fetch");
+const { query } = require("./util/hasura");
 
 const movies = require("../data/movies.json");
 
 exports.handler = async () => {
-  const api = new URL("https://www.omdbapi.com");
-
-  api.searchParams.set("apikey", process.env.OMDB_API_KEY);
-
-  const promises = movies.map((movie) => {
-    api.searchParams.set("i", movie.id);
-
-    return fetch(api)
-      .then((res) => res.json())
-      .then((data) => {
-        const scores = data.Ratings;
-
-        return {
-          ...movie,
-          scores,
-        };
-      });
+  const { movies } = await query({
+    query: `
+      query {
+        movies {
+          id
+          poster
+          tagline
+          title
+        }
+      }
+    `,
   });
 
-  const moviesWithRatings = await Promise.all(promises);
+  // const api = new URL("https://www.omdbapi.com");
+
+  // api.searchParams.set("apikey", process.env.OMDB_API_KEY);
+
+  // const promises = movies.map((movie) => {
+  //   api.searchParams.set("i", movie.id);
+
+  //   return fetch(api)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       const scores = data.Ratings;
+
+  //       return {
+  //         ...movie,
+  //         scores,
+  //       };
+  //     });
+  // });
+
+  // const moviesWithRatings = await Promise.all(promises);
 
   return {
     statusCode: 200,
-    body: JSON.stringify(moviesWithRatings),
+    body: JSON.stringify(movies),
   };
 };
