2d91db1449080962d268b96e3dea4370f7c3b296
Sebastian <sebastian.sloniec@gmail.com>
2022-02-19 19:51:19 +0100

    VUE: Add filtering restaurants with computed properties

--- 03-frontend-masters/20-building-apps-vue/nuxtfoodapp/pages/index.vue
+++ 03-frontend-masters/20-building-apps-vue/nuxtfoodapp/pages/index.vue
@@ -1,11 +1,13 @@
 <template>
   <main class="container">
     <AppHeader />
-    <AppRestaurantInfo />
+    <AppRestaurantInfo :datasource="fooddata" />
   </main>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 import AppHeader from "@/components/AppHeader.vue";
 import AppRestaurantInfo from "../components/AppRestaurantInfo.vue";
 
@@ -13,6 +15,9 @@ export default {
   components: {
     AppHeader,
     AppRestaurantInfo,
+  },
+  computed: {
+    ...mapState(["fooddata"]),
   },
 };
 </script>
