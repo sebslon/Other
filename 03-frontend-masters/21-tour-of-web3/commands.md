48c0fbcbfc0080095b27e3b91312367b34647ab8
Sebastian <sebastian.sloniec@gmail.com>
2022-02-22 19:49:51 +0100

    WEB3: deploy another contract, gas estimations etc.

---
+++ 03-frontend-masters/21-tour-of-web3/commands.md
@@ -0,0 +1,12 @@
+https://github.com/ThePrimeagen/web3-smart-contracts
+
+npx webpack
+npx hardhat node // run blockchain
+npx hardhat test
+npx hardhat run scripts/deploy-counter.ts --network localhost
+
+nonce too high / - metamask -> settings -> advanced -> reset account
+
+Alchemy/Infura - deployment to live network
+
+gas cost _ gas used _ 10^9 _ eth-cost / 10^18 or gas-cost _ gas-used \* eth-cost / 10^9
--- 03-frontend-masters/21-tour-of-web3/hardhat.config.ts
+++ 03-frontend-masters/21-tour-of-web3/hardhat.config.ts
@@ -1,3 +1,4 @@
+import "hardhat-gas-reporter";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
 /**
@@ -5,4 +6,13 @@ import "@nomiclabs/hardhat-ethers";
  */
 module.exports = {
   solidity: "0.8.10",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`${process.env.RINKEBY_KEY}`],
+    },
+  },
 };
