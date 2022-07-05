e00395ea63da98a8da2d813b6d78ac5868a0a24c
Sebastian <sebastian.sloniec@gmail.com>
2022-07-05 21:15:55 +0200

    CRON-MAILER: initial setup of backend

---
+++ 00-gladiatorsofjs-exercises/03-Backend-Express-Next-TS/1.Server/solutions/4-CronMailSender/backend/index.ts
@@ -0,0 +1,7 @@
+import { App } from "./app";
+
+export const server = new App([
+  /* routers */
+]);
+
+server.listen();
---
+++ 00-gladiatorsofjs-exercises/03-Backend-Express-Next-TS/1.Server/solutions/4-CronMailSender/backend/src/types/index.ts
@@ -0,0 +1,6 @@
+import { Router as ExpressRouter } from "express";
+
+export interface IRouter {
+  path: string;
+  router: ExpressRouter;
+}
