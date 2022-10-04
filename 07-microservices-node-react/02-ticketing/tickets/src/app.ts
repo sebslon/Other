836ae9014f69c267ffe12d6bb81405c9fa304abb
Sebastian <sebastian.sloniec@gmail.com>
2022-10-04 09:41:39 +0200

    MSVCS-2(tickets): Finish tickets service routes

--- 07-microservices-node-react/02-ticketing/tickets/src/app.ts
+++ 07-microservices-node-react/02-ticketing/tickets/src/app.ts
@@ -5,6 +5,9 @@ import cookieSession from 'cookie-session';
 import { currentUser, errorHandler, NotFoundError } from '@msvcs/common';
 
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 
@@ -18,6 +21,9 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', () => {
   throw new NotFoundError();
--- 07-microservices-node-react/02-ticketing/tickets/src/tests-configs/auth-test-helper.ts
+++ 07-microservices-node-react/02-ticketing/tickets/src/tests-configs/auth-test-helper.ts
@@ -1,8 +1,12 @@
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
 
 export class AuthTestHelper {
   static signin = async () => {
-    const payload = { id: '123', email: 'test@test.com' };
+    const payload = {
+      id: new mongoose.Types.ObjectId().toHexString(),
+      email: 'test@test.com',
+    };
     const token = jwt.sign(payload, process.env.JWT_KEY!);
     const session = { jwt: token };
     const sessionJSON = JSON.stringify(session);
