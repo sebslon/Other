49cbb60246cf78b57313e317181232b8d4105a88
Sebastian <sebastian.sloniec@gmail.com>
2022-10-11 14:19:09 +0200

    MSVCS(expiration): Emit expiration events and listen in other services

--- 07-microservices-node-react/02-ticketing/orders/src/index.ts
+++ 07-microservices-node-react/02-ticketing/orders/src/index.ts
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
 
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
+
+import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener';
 import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
 import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
-import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
@@ -31,6 +33,7 @@ const start = async () => {
 
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
+    new ExpirationCompleteListener(natsWrapper.client).listen();
 
     await mongoose
       .connect(process.env.MONGO_URI)
